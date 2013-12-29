713f2c243ac05686831c599e0a4eebc035d32399
Dane Petersen <thegreatsunra@gmail.com>
2013-12-29 15:20:07 -0800

    Update start of file to be consistent with generator-angular

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -1,6 +1,17 @@
-module.exports = function(grunt) {
-  'use strict';
+'use strict';
 
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
   grunt.initConfig({
